c12bbb39d87f14e1a47c3e3c6a4ff5291763606a
antonio <antoniorocca46@gmail.com>
2020-12-07 19:38:43 +0100

    Third Commit

--- seconda-parte/jsnack-5/js/main.js
+++ seconda-parte/jsnack-5/js/main.js
@@ -4,3 +4,16 @@
 // La funzione ritornerà un nuovo array con i valori che
 // hanno la posizione compresa tra “a” e “b”
 
+var numeri = [1, 2, 3, 4, 5, 6];
+var primo = 2;
+var secondo = 6;
+
+function treArgomenti(array, a, b) {
+    var nuovoArray = []; 
+    for (var i = a - 1; i < b; i++) {
+        nuovoArray.push(array[i]); 
+    }
+    return nuovoArray
+}
+
+console.log(treArgomenti(numeri, primo, secondo));
